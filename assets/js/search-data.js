27beb2aa4fa4e59f5c9bef6f5323e724a90c29a3
JustinJZhang <75781829+JustinJZhang@users.noreply.github.com>
2026-09-09 15:59:31 +0000

    Deploying to gh-pages from @ JustinJZhang/justinjzhang.github.io@1c196041e03d13f78c87d7122785c1c92e08c2f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -110,20 +110,6 @@ ninja.data = [{
           window.open("https://ieeexplore.ieee.org/author/37089862108/", "_blank");
         },
       },{
-        id: 'social-orcid',
-        title: 'ORCID',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://orcid.org/0000-0002-4981-3534", "_blank");
-        },
-      },{
-        id: 'social-whatsapp',
-        title: 'whatsapp',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://wa.me/85269302942", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
