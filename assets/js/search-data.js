583f54c80e87f4b44b2ec73913b413110e4fb2b2
JustinJZhang <75781829+JustinJZhang@users.noreply.github.com>
2026-09-09 15:28:49 +0000

    Deploying to gh-pages from @ JustinJZhang/justinjzhang.github.io@a272c6c55a309a3a1921f9edf4c39e030bc04583 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -103,6 +103,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=eAh9ne8AAAAJ", "_blank");
         },
       },{
+        id: 'social-ieee',
+        title: 'IEEE Xplore',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://ieeexplore.ieee.org/author/ieeexplore.ieee.org/author/37089862108/", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
